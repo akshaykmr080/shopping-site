20ef1593a360195a8d045552535a635eabf140f6
Akshay Kumar Shreekanth <akshay.kumar.shreekanth@sap.com>
2019-07-10 15:13:26 -0700

    added new recipe items

--- src/app/recepies/recipe.service.ts
+++ src/app/recepies/recipe.service.ts
@@ -1,14 +1,19 @@
 import { Recepie } from './recepie.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 export class RecipeService {
 
     private recipes: Recepie[] = [
-        new Recepie('A Test Recepie 1', 'test', 
-        'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2018/07/Cheap-family-meals-Recipes-under-%C2%A31-per-head-920x605.jpg'),
-        new Recepie('A Test Recepie 2', 'test', 
-        'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2018/07/Cheap-family-meals-Recipes-under-%C2%A31-per-head-920x605.jpg')
+        new Recepie('Chicken Tandoori',
+        'Spicy, tasty tandoori chicken', 
+        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chicken-tandori-1526595014.jpg?resize=480:*', 
+        [new Ingredient('Chicken', 1), new Ingredient('French fries', 20)]),
+
+        new Recepie('Mango Lassi', 'Lipsmacking mango treat', 
+        'https://www.simplyrecipes.com/wp-content/uploads/2006/11/mango-lassi-vertical-a-1200.jpg', 
+        [new Ingredient('Mango', 4), new Ingredient('Sugas', 2)])
       ]
 
     recipeSelected = new EventEmitter<Recepie>()
